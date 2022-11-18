199836b96fdb1be3ca4e3b39fadfded95ee9cbea
Kenneth Blossom's Portfolio <33361957+mscrnt@users.noreply.github.com>
2022-11-17 17:53:57 -0800

    Grimm DL

--- grimmDL.py
+++ grimmDL.py
@@ -1,25 +1,33 @@
+
+"""
+This program downloads the 1st 100 stories of The Brother's Grimm and saves it in your working folder.
+"""
+
 import os
-
-#create a directory if needed
-
-dir_name = "stories"
-url = "https://www.cs.cmu.edu/~spok/grimmtmp/"
-
-if not os.path.exists(dir_name):
-    os.mkdir(dir_name)
-    def maybe_download(filename):
-        """Download a file if not present"""
-        print("Downloading file: ", dir_name+ os.sep+filename)
-if not os.path.exists(dir_name+os.sep+filename):
-    filename, _ = urlretrieve(url + filename, dir_name+os.sep+filename)
-else:
-    print("File ", filename, "already exists.")
+import requests
 
 
-num_files = 100
+def download(url: str, dest_folder: str):
 
-filenames = [format(i, '03d')+'.txt' for i in range(1,101)]
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)  # creates "stories" directory if it does not exist
 
-for fn in filenames:
 
-maybe_download(fn)
+    for i in range(1, 101): #repeats download 100 times
+        num = f"{i:03}" #converts iteration into triple digits to match website
+        filename = url + str(num) + ".txt" # 
+        save_location = dest_folder + str(num) + ".txt"
+        r = requests.get(filename, stream=True) #makes download request with converted url w/ filename
+        if r.ok:
+            print("saving to", os.path.abspath(save_location))
+            with open(save_location, 'wb') as f: #saving file to hdd.
+                for chunk in r.iter_content(chunk_size=1024 * 8):
+                    if chunk:
+                        f.write(chunk)
+                        f.flush()
+                        os.fsync(f.fileno())
+        else:  # HTTP status code 4XX/5XX
+            print("Download failed: status code {}\n{}".format(r.status_code, r.text))
+
+
+download("https://www.cs.cmu.edu/~spok/grimmtmp/", dest_folder="stories/")
