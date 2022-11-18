2c4b659bf9e3f284439eb810ade35c527791c7e5
Kenneth Blossom's Portfolio <33361957+mscrnt@users.noreply.github.com>
2022-11-17 19:25:13 -0800

    Training Examples

--- train.py
+++ train.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
-
 import numpy as np
 import os
 import time
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+
 
 # Read, then decode for py2 compat.
 text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
@@ -18,7 +19,51 @@ print(text[:250])
 vocab = sorted(set(text))
 print(f'{len(vocab)} unique characters')
 
-example_texts = ['abcdefg', 'xyz']
+chars = tf.strings.unicode_split(path_to_file, input_encoding='UTF-8')
+print(chars[:10])
 
-chars = tf.strings.unicode_split(example_texts, input_encoding='UTF-8')
-chars
+ids_from_chars = tf.keras.layers.StringLookup(
+    vocabulary=list(vocab), mask_token=None)
+
+ids = ids_from_chars(chars)
+print(ids[:10])
+
+chars_from_ids = tf.keras.layers.StringLookup(
+    vocabulary=ids_from_chars.get_vocabulary(), invert=True, mask_token=None)
+
+chars = chars_from_ids(ids)
+print(chars[:10])
+
+def text_from_ids(ids):
+  return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
+
+all_ids = ids_from_chars(tf.strings.unicode_split(text, 'UTF-8'))
+print(all_ids)
+
+ids_dataset = tf.data.Dataset.from_tensor_slices(all_ids)
+
+for ids in ids_dataset.take(10):
+    print(chars_from_ids(ids).numpy().decode('utf-8'))
+
+seq_length = 100
+
+sequences = ids_dataset.batch(seq_length+1, drop_remainder=True)
+
+for seq in sequences.take(1):
+  print(chars_from_ids(seq))
+
+for seq in sequences.take(5):
+  print(text_from_ids(seq).numpy())
+
+def split_input_target(sequence):
+    input_text = sequence[:-1]
+    target_text = sequence[1:]
+    return input_text, target_text
+
+print(split_input_target(list("Tensorflow")))   
+
+dataset = sequences.map(split_input_target)
+
+for input_example, target_example in dataset.take(1):
+    print("Input :", text_from_ids(input_example).numpy())
+    print("Target:", text_from_ids(target_example).numpy())
